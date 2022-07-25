b5bbba50bca468a1c90834d3960e61f31f1db350
ssamkkim <ssamuel.kkim@gmail.com>
2022-07-25 02:05:28 -0700

    Fetched anime api and displayed in a 6 column grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
 
-function App() {
+const App = () => {
+  //state
+  const [animeData, setAnimeData] = useState([]);
+  //fetch animeData
+  const fetchAnimeData = () => {
+    fetch('https://api.jikan.moe/v4/anime')
+      .then(result => result.json())
+      .then(data => setAnimeData(data.data))
+      .catch(error => console.log(error));
+  };
+  useEffect(() => {
+    fetchAnimeData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Anime</h2>
+      <div id="anime-container">
+        {animeData.map((n, i) => (
+          <div key={i}>
+            <p>{n.title}</p>
+            <img src={n.images.webp.image_url} alt={n.title + " image"} className="anime-image" />
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
