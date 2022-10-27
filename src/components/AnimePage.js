3bae300781b5e2070cd9e0a53016adf067871f58
ssamkkim <ssamuel.kkim@gmail.com>
2022-10-26 23:48:11 -0700

    Simple Anime Page Information

--- src/components/AnimePage.js
+++ src/components/AnimePage.js
@@ -24,11 +24,31 @@ const AnimePage = () => {
       })
   }
 
+  const animeAiringDate = () => {
+    const airingText = animeData.airing ? 'Airing' : 'Aired';
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const fromDate = `${monthNames[animeData.aired.prop.from.month - 1]} ${animeData.aired.prop.from.day}, ${animeData.aired.prop.from.year}`;
+    const toDate = animeData.airing ? '?' : `${monthNames[animeData.aired.prop.to.month -1]} ${animeData.aired.prop.to.day}, ${animeData.aired.prop.to.year}`;
+
+    return (
+      <p>{`${airingText}: ${fromDate} - ${toDate}`}</p>
+    )
+  }
+
   const displayAnimeInfo = () => {
     return (
       <div>
         <h1>{animeData.title}</h1>
         <img src={animeData.images.webp.image_url} alt={animeData.title} />
+        <div>
+          <h1>Information</h1>
+          <p>{`Episodes: ${animeData.episodes}`}</p>
+          <p>{`Status: ${animeData.status}`}</p>
+          <p>{animeAiringDate()}</p>
+          <p>{`Episode Duration: ${animeData.duration.substr(0,6)}.`}</p>
+          <p>{`Animation Studio: ${animeData.studios[0].name}`}</p>
+          <p>{animeData.demographics === undefined ? `Demographic: ${animeData.demographics[0].name}` : null}</p>
+        </div>
       </div>
     )
   }
