1f764965d9dc6253573ad0c1f1af2b1fc403d242
ssamkkim <ssamuel.kkim@gmail.com>
2022-10-30 01:48:57 -0700

    Basic Passport Setup

---
+++ server/passport.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+import User from './models/User.js';
+
+export default function() {
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+  },
+  async(accessToken, refreshToken, profile, done) => {
+    const newUser = {
+      googleId: profile.id,
+      userName: profile.displayName,
+      image: profile.photos[0].value
+    };
+  
+    try {
+      let user = await User.findOne({ googleId: profile.id });
+  
+      if(user) {
+        done(null, user);
+      } else {
+        user = await User.create(newUser);
+        done(null, user);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }))
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => done(err, user))
+  })
+}
