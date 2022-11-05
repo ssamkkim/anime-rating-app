a6334bce270eb684e8cf6f7ba6759483e769049a
ssamkkim <ssamuel.kkim@gmail.com>
2022-11-05 00:58:11 -0700

    Active Search Resutls/Absolute Display

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { AiOutlineSearch } from 'react-icons/ai';
@@ -7,6 +7,21 @@ import { AiOutlineSearch } from 'react-icons/ai';
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchList, setSearchList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.jikan.moe/v4/anime?q=${searchQuery}&order_by=popularity`
+      )
+      .then((response) => {
+        setSearchList(
+          response.data.data.filter((anime) => anime.score > 0).slice(0, 5)
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [searchQuery]);
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -27,7 +42,7 @@ const SearchBar = () => {
 
   const displaySearchAnime = () => {
     return (
-      <div className="text-white">
+      <div className="text-white absolute top-14">
         {searchList.map((anime) => (
           <div
             className="p-2 text-white hover:text-blue-400"
@@ -52,7 +67,7 @@ const SearchBar = () => {
 
   return (
     <div className="flex">
-      <form onSubmit={handleSearch}>
+      <form className="flex flex-col" onSubmit={handleSearch}>
         <input
           className="rounded-full bg-black outline outline-1 outline-gray-500 py-2 px-5 w-96 text-white focus:outline-blue-500"
           type="search"
